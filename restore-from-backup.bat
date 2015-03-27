89f2a2754ac1c5e9c178d2b522e1c130c0aee851
Eric Steinborn <esteinborn@gmail.com>
2015-03-27 16:19:46 -0400

    Adding Restore from Backup script

---
+++ restore-from-backup.bat
@@ -0,0 +1,29 @@
+@echo off
+
+if exist %HOMEDRIVE%%HOMEPATH%\.npmrc (
+  copy backup\npm.txt %HOMEDRIVE%%HOMEPATH%\.npmrc /y
+)
+
+if exist %HOMEDRIVE%%HOMEPATH%\.bowerrc (
+  copy backup\bower.txt %HOMEDRIVE%%HOMEPATH%\.bowerrc /y
+)
+
+if exist %HOMEDRIVE%%HOMEPATH%\.curlrc (
+  copy backup\curl.txt %HOMEDRIVE%%HOMEPATH%\.curlrc /y
+)
+
+if exist %HOMEDRIVE%%HOMEPATH%\.wgetrc (
+  copy backup\wget.txt %HOMEDRIVE%%HOMEPATH%\.wgetrc /y
+)
+
+if exist %HOMEDRIVE%%HOMEPATH%\.gitconfig (
+  copy backup\git.txt %HOMEDRIVE%%HOMEPATH%\.gitconfig /y
+)
+
+if exist %HOMEDRIVE%%HOMEPATH%\.ssh\config (
+  copy backup\ssh.txt %HOMEDRIVE%%HOMEPATH%\.ssh\config /y
+)
+
+if exist c:\ruby193\bin\gem.bat (
+  copy backup\ruby.txt c:\ruby193\bin\gem.bat /y
+)
